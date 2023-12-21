13ecb1b9c002cb63afd75641af55ea4ef8d2ced4
achuta <sreekath.achuta@gmail.com>
2023-12-21 20:29:56 +0530

    adding topics and doing scripting roboshop ngnix

---
+++ frontend.sh
@@ -0,0 +1,8 @@
+dnf install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+vim /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemctl start nginx
